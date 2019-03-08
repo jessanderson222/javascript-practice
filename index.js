54a83f3498e2c0dfc751c80e3339bafe9a93cc1a
jessanderson222 <jessanderson222@gmail.com>
2019-03-07 21:50:38 -0500

    can drag box around but cannot drop it anywhere

---
+++ index.js
@@ -0,0 +1,9 @@
+// const box = document.getElementsByClassName("box")[0];
+
+// const containers = document.getElementsByClassName("holder");
+
+// for (const container of containers) {
+//   container.addEventListener("dragover", dragover);
+//   container.addEventListener("dragenter", dragenter);
+//   container.addEventListener("drop", drop);
+// }
